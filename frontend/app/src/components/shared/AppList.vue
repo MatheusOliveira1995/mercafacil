3bcd7eb9ffa8d29b65f705688daa07b080e9f3ee
MatheusOliveira <mateusribeirooliveira@hotmail.com>
2021-11-22 21:13:53 -0300

    fixed pagination

--- frontend/app/src/components/shared/AppList.vue
+++ frontend/app/src/components/shared/AppList.vue
@@ -6,7 +6,7 @@
         {{ title }}
       </q-toolbar-title>
       <SearchBar @search-bar:search="performFilter($event)" />
-      <q-btn @click="sortList()" flat round dense icon="sort_by_alpha" />
+      <q-btn @click="performSort()" flat round dense icon="sort_by_alpha" />
     </q-toolbar>
     <q-item v-for="item in showing" :key="item.id">
       <q-item-section>
@@ -22,7 +22,7 @@
 </template>
 
 <script lang="ts">
-import { ref, SetupContext, watch } from 'vue'
+import { ref, SetupContext, watch, onMounted } from 'vue'
 import SearchBar from './SearchBar.vue'
 /**
  */
@@ -67,16 +67,16 @@ export default {
     const storedPage = parseInt(localStorage.getItem('currentPage') ?? '1')
     localStorage.removeItem('currentPage')
     const currentPage = ref(storedPage)
-    let ascending = true
+    const ascending = ref(localStorage.getItem('ordering') === 'asc')
     const sorter = (a: unknown, b: unknown) => {
       const item1 = a as SortItem
       const item2 = b as SortItem
       if (item1.name > item2.name) {
-        return ascending ? 1 : -1
+        return ascending.value ? 1 : -1
       }
 
       if (item1.name < item2.name) {
-        return ascending ? -1 : 1
+        return ascending.value ? -1 : 1
       }
 
       return 0
@@ -84,23 +84,36 @@ export default {
     const performFilter = (filter: string) => {
       emit('app-list:filter', filter)
     }
+    const performSort = () => {
+      ascending.value = !ascending.value
+      sortList()
+    }
     const sortList = () => {
-      ascending = !ascending
       showing.value = showing.value.sort(sorter)
     }
     watch(currentPage, () => {
-      localStorage.setItem(
-        'currentPage',
-        currentPage.value ? currentPage.value.toString() : '1'
-      )
+      localStorage.setItem('currentPage', currentPage.value ? currentPage.value.toString() : '1')
       emit('app-list:pageChange', currentPage.value)
+    })
+    watch(ascending, () => {
+      if (ascending.value) { localStorage.setItem('ordering', 'asc'); return }
+      localStorage.setItem('ordering', 'desc')
+    })
+    onMounted(() => {
+      if (!localStorage.getItem('ordering')) return
+      sortList()
     })
     return {
       currentPage,
       performFilter,
       showing,
-      sortList
+      performSort
     }
+  },
+  /**
+   */
+  unmounted () {
+    if (!localStorage.getItem('ordering')) { localStorage.removeItem('ordering') }
   }
 }
 </script>
